ccf3980201da68abbbc467810ea88babdbb1b6d0
Nehavinamra <vinamraneha@gmail.com>
2024-02-29 19:42:51 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require ('express');
-const app = express();
-const port = 3000;
+const cors = require ('cors');
 
+const app = express();
+app.use('cors()');
+
+const port = 3000;
 app.get('/',(req,res) => {res.send('Arithmetic service - Hello World!');});
 app.listen(port);
